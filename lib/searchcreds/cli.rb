bb0a81cfbbe0f44a848e0605009dfbc499776425
krishpranav <krisna.pranav@gmail.com>
2021-07-01 12:06:12 +0530

    [+] opts [+]

--- lib/searchcreds/cli.rb
+++ lib/searchcreds/cli.rb
@@ -9,4 +9,13 @@ module Searchcreds
         class CredentialsFileError < Searchcreds::CLI::Error; end
         class CredentialsFileNotReadable < Searchcreds::CLI::CredentialsFileError; end
         class CredentialsFileCorrupt < Searchcreds::CLI::CredentialsFileError; end
-        
+
+        def self.run!(args)
+            @options = OpenStruct.new
+            @options.case_sensitive = false
+            @options.exact = false
+            @opt_parse = OptionParse.new do |opts|
+                opts.banner = "Usage: #{PROGRAM_NAME} [options] term [term2] ... [termN]"
+                opts.program_name = PROGRAM_NAME
+                opts.separator ""
+                opts.separator "Specific options:"
