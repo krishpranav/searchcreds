1ff4c852a6b81e0cd56f6230060dc3c279315e6a
krishpranav <krisna.pranav@gmail.com>
2021-07-01 11:59:45 +0530

    [+] search creds specs [+]

---
+++ spec/searchcreds_spec.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'spec_helper'
+
+describe Searchcreds do
+    it 'has a version number' do
+        expect(Searchcreds::VERSION).not_to be nil
+    end
+
+    it 'does something useful' do
+        expect(false).to eq(true)
+    end
+end
