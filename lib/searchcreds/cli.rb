2af4573c2df814e207d6510c7c6c74aa4ce4b049
krishpranav <krisna.pranav@gmail.com>
2021-07-01 12:03:21 +0530

    [+] classes [+]

--- lib/searchcreds/cli.rb
+++ lib/searchcreds/cli.rb
@@ -4,4 +4,9 @@ module Searchcreds
         CREDENTIALS_FILE    = File.join(File.dirname(__FILE__), "..", "..", "credentials.json")
         CREDENTIALS_SEPARATOR = "========================================================"
 
+
+        class Error < StandartError; end
+        class CredentialsFileError < Searchcreds::CLI::Error; end
+        class CredentialsFileNotReadable < Searchcreds::CLI::CredentialsFileError; end
+        class CredentialsFileCorrupt < Searchcreds::CLI::CredentialsFileError; end
         
